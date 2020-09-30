585c417a2173b2ca35043849acc20219eb97b50e
Laura Buonincontri INFN Padova <buoninco@ilc-laura.novalocal>
2020-09-30 11:19:47 +0200

    Simulation steering file

--- tutorial/1-mumuHHbbbb/sim_steer_Hbb.py
+++ tutorial/1-mumuHHbbbb/sim_steer_Hbb.py
@@ -18,7 +18,7 @@ SIM.inputFiles = ["/data/samples/HH/mumu2H2bb750.stdhep"]
 ## Macro file to execute for runType 'run' or 'vis'
 SIM.macroFile = ""
 ## number of events to simulate, used in batch mode. -1 all
-SIM.numberOfEvents = 10
+SIM.numberOfEvents = 1
 ## Outputfile from the simulation,only lcio output is supported
 SIM.outputFile = "mumu_H_bb.slcio"
 ## Verbosity use integers from 1(most) to 7(least) verbose
