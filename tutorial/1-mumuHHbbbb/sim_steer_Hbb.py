0acc55d44a2cfa8f3aa87cf2d595d11f6c673457
sslomte <86433874+sslomte@users.noreply.github.com>
2021-07-26 19:07:39 -0500

    Update sim_steer_Hbb.py

--- tutorial/1-mumuHHbbbb/sim_steer_Hbb.py
+++ tutorial/1-mumuHHbbbb/sim_steer_Hbb.py
@@ -21,7 +21,7 @@ SIM.inputFiles = ["bbbb_3tev_whizard.stdhep"]
 ## Macro file to execute for runType 'run' or 'vis'
 SIM.macroFile = ""
 ## number of events to simulate, used in batch mode. -1 all
-SIM.numberOfEvents = 100
+SIM.numberOfEvents = 2
 ## Outputfile from the simulation,only lcio output is supported
 SIM.outputFile = "mumu_HH_bbbb_3tev.slcio"
 ## Verbosity use integers from 1(most) to 7(least) verbose
